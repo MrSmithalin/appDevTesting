918bc9433b7a0b2d0f31c714e5e4984321440b24
alecmcarosmith <145041997+alecmcarosmith@users.noreply.github.com>
2024-11-24 22:14:30 -0500

    Initial Commit

---
+++ appDevTesting/appDevTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  appDevTestingApp.swift
+//  appDevTesting
+//
+//  Created by Alec Smith on 11/24/24.
+//
+
+import SwiftUI
+
+@main
+struct appDevTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ appDevTestingTests/appDevTestingTests.swift
@@ -0,0 +1,17 @@
+//
+//  appDevTestingTests.swift
+//  appDevTestingTests
+//
+//  Created by Alec Smith on 11/24/24.
+//
+
+import Testing
+@testable import appDevTesting
+
+struct appDevTestingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
